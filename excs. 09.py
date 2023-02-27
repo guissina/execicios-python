844cb52c46c5ca5f85af2f2b35032d1af691dc37
Guilherme Messina <109996764+guissina@users.noreply.github.com>
2023-02-27 18:38:30 -0300

    Update excs. 09.py

--- excs. 09.py
+++ excs. 09.py
@@ -1,4 +1,16 @@
-nome = input("digite um nome completo: ")
-nome = nome.split()
-print(nome[0])
-print(nome[-1])
+#nome = input("digite um nome completo: ")
+#nome = nome.split()
+#print(nome[0])
+#print(nome[-1])
+
+#procura silva no nome
+#nome = str(input('digite um nome: ')).strip()
+#print('Analisando seu nome...')
+#print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+
+#procura 'Santo'
+cidade = str(input("Digite uma cidade: ")).strip()
+print('Essa cidade tem Santo no nome? {}'.format('santo' in cidade.lower()))
+cidade = cidade.lower()
+cidade = cidade.split()
+print('Tem Santo no primeiro nome? {}'.format('santo' in cidade[0]))
